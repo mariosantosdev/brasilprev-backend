bd7bd5572f18054ea6a432fe6a800a29ee7a5ca0
Mário Santos <mariodev7@gmail.com>
2024-05-24 01:54:29 -0300

    chore: setup class-validator and class-transform

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe({ errorHttpStatusCode: 422 }));
+
+  app.useGlobalInterceptors(
+    new ClassSerializerInterceptor(app.get(Reflector), {
+      strategy: 'excludeAll',
+      excludeExtraneousValues: true,
+    }),
+  );
+
   await app.listen(3000);
 }
 bootstrap();
