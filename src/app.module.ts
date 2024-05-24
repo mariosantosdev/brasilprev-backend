6a273f041d8ff7f90b3ccaa5f8e25c78d4ae584b
Mário Santos <mariodev7@gmail.com>
2024-05-24 01:52:12 -0300

    chore: remove initial controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,14 @@
 import { Module } from '@nestjs/common';
-import { HelloWorldController } from './controllers/hello-world/hello-world.controller';
-import { HelloWorldService } from './services/hello-world/hello-world.service';
+import { DatabaseModule } from './infra/database/database.module';
 import { RegisterClientService } from './application/services/register-client/register-client.service';
 import { RegisterProductService } from './application/services/register-product/register-product.service';
 import { HirePlanService } from './application/services/hire-plan/hire-plan.service';
 import { InvestExtraContributionService } from './application/services/invest-extra-contribution/invest-extra-contribution.service';
 import { WithdrawContributionService } from './application/services/withdraw-contribution/withdraw-contribution.service';
-import { DatabaseModule } from './infra/database/database.module';
 
 @Module({
   imports: [DatabaseModule],
-  controllers: [HelloWorldController],
   providers: [
-    HelloWorldService,
     RegisterClientService,
     RegisterProductService,
     HirePlanService,
