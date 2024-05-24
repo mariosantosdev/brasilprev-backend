bc644522b44e2d27c8e030d94e033193cc88402a
Mário Santos <mariodev7@gmail.com>
2024-05-23 21:23:29 -0300

    test(factories): client factory receives overrides

--- test/factories/make-client.factory.ts
+++ test/factories/make-client.factory.ts
@@ -2,15 +2,26 @@ import { ClientEntity } from '@/application/entities/client.entity';
 import { cpf } from 'cpf-cnpj-validator';
 import { faker } from '@faker-js/faker';
 
-export function makeClientFactory(): ClientEntity {
-  const client = ClientEntity.create({
-    name: faker.person.fullName(),
-    cpf: cpf.generate(),
-    birthDate: faker.date.past(),
-    email: faker.internet.email(),
-    gender: faker.person.gender(),
-    monthlyIncome: faker.number.float({ fractionDigits: 2 }),
-  });
+type OverridesParams = Partial<Omit<ClientEntity, 'age'>>;
+
+export function makeClientFactory(overrides?: OverridesParams): ClientEntity {
+  const client = ClientEntity.create(
+    {
+      name: overrides?.name ?? faker.person.fullName(),
+
+      cpf: overrides?.cpf ?? cpf.generate(),
+
+      birthDate: overrides?.birthDate ?? faker.date.past(),
+
+      email: overrides?.email ?? faker.internet.email(),
+
+      gender: overrides?.gender ?? faker.person.gender(),
+
+      monthlyIncome:
+        overrides?.monthlyIncome ?? faker.number.float({ fractionDigits: 2 }),
+    },
+    overrides?.id,
+  );
 
   return client.value as ClientEntity;
 }
